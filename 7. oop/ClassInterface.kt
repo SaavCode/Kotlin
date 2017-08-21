a41b313e4f9d3479dbeac8862c7eaecc0458e49d
Nestor Saavedra <31100486+saavfoxdev@users.noreply.github.com>
2017-08-21 14:58:46 -0700

    Update ClassInterface.kt

--- 7. oop/ClassInterface.kt
+++ 7. oop/ClassInterface.kt
@@ -1,22 +1,23 @@
 
 interface op{
-    fun sum(n1:Int,n2:Int) { println("sum:"+ n1+n2)}
-    fun div(n1:Int,n2:Int) { println("div:"+ n1/n2)}
+    fun sum(n1:Int,n2:Int) { println("sum:"+ n1+n2)}//summation, takes two numbers
+    fun div(n1:Int,n2:Int) { println("div:"+ n1/n2)}//division, takes println by
+                    //default if given implementation
 }
 
 
-class UserOp:op{
-    override fun sum(n1: Int, n2: Int) {
+class UserOp:op{//needs to implement two methods
+    override fun sum(n1: Int, n2: Int) {//override original function
         println("sum:"+ n1+n2)
       }
 
-    override fun div(n1: Int, n2: Int) {
+    override fun div(n1: Int, n2: Int) {//override division function
         println("div:"+ n1/n2)
     }
 
 }
 
-class AdminOp:op{
+class AdminOp:op{//override functions again
     override fun sum(n1: Int, n2: Int) {
         println("sum:"+ (n1+n2-2))
     }
