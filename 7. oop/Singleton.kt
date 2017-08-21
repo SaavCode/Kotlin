8f6050e2ffcdc9a96db31cd9ca7335863c4251a7
Nestor Saavedra <31100486+saavfoxdev@users.noreply.github.com>
2017-08-21 16:38:20 -0700

    Update Singleton.kt

--- 7. oop/Singleton.kt
+++ 7. oop/Singleton.kt
@@ -1,24 +1,24 @@
 
 class  Singleton{
     var name:String?=null
-   private constructor(){
+   private constructor(){//dont allow instance
         println("instance is created")
     }
 
     companion object {
-        val instance:Singleton by lazy { Singleton() }
+        val instance:Singleton by lazy { Singleton() }//sends previous
     }
 }
 
 fun  main(args:Array<String>){
 
-    var s1=Singleton.instance
-    s1.name="hussein"
-    println(s1.name)
+    var s1=Singleton.instance 
+    s1.name="nestor"
+    println(s1.name)//prints name
 
-    var s2=Singleton.instance
-    println(s2.name)
+    var s2=Singleton.instance//gets instancce 1
+    println(s2.name)       
 
-    var s3=Singleton.instance
+    var s3=Singleton.instance//gest instance 1
     println(s3.name)
 }
