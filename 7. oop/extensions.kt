a2ac95de1a8e12167838e2eaf804aff5e70074c7
Nestor Saavedra <31100486+saavfoxdev@users.noreply.github.com>
2017-08-21 16:22:36 -0700

    Update extensions.kt

--- 7. oop/extensions.kt
+++ 7. oop/extensions.kt
@@ -1,18 +1,23 @@
 
 
- fun ArrayList<String>.swap(index1:Int,index2:Int){
-     var temp=this.get(index1)
-     this.set(index1,this.get(index2))
+ fun ArrayList<String>.swap(index1:Int,index2:Int){//swap index1 and 2
+     var temp=this.get(index1)//get index1 fromn this class
+     this.set(index1,this.get(index2))//swap index1 and 2
+  //swap with temp
      this.set(index2,temp)
  }
 
 fun main(args:Array<String>){
         var arrayList=ArrayList<String>()
-        arrayList.add("hussein")
-        arrayList.add("jena")
-        arrayList.add("laya")
+ //add to array
+        arrayList.add("nestor")
+        arrayList.add("mel")
+        arrayList.add("milo")
+ //print list
         println(arrayList)
+ //swap elements in the array
         arrayList.swap(0,1)
+ //print list after swap
         println(arrayList)
 
 
